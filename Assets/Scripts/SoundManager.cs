6723473bf7cbd6d5ae2285c85af9bda0b350de98
hunte <besscomdark@gmail.com>
2023-08-08 07:01:29 +0800

    添加背景音樂

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    private AudioSource aud;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+}
