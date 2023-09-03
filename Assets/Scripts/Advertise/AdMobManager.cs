395b389219606e1bbcd9db6fa7c742af125a163f
hunte <besscomdark@gmail.com>
2023-09-03 22:03:25 +0800

    廣告置入

---
+++ Assets/Scripts/Advertise/AdMobManager.cs
@@ -0,0 +1,29 @@
+﻿using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class AdMobManager : MonoBehaviour
+{
+    private BannerView bannerView;
+
+    private void Start()
+    {
+        // 初始化 AdMob SDK
+        MobileAds.Initialize(initStatus => { });
+
+        // 創建一個橫幅廣告
+        bannerView = new BannerView("YOUR_BANNER_AD_UNIT_ID", AdSize.Banner, AdPosition.Bottom);
+
+        // 載入並顯示橫幅廣告
+        AdRequest request = new AdRequest.Builder().Build();
+        bannerView.LoadAd(request);
+    }
+
+    private void OnDestroy()
+    {
+        // 在銷毀時釋放橫幅廣告
+        if (bannerView != null)
+        {
+            bannerView.Destroy();
+        }
+    }
+}
